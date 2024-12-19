695224d2792b5a1a006bcd98e956a418c4c18ca9
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-12-19 13:59:13 +0900

    Update script.js

--- w_player/v20/script.js
+++ w_player/v20/script.js
@@ -234,16 +234,20 @@
         startStopButton.addEventListener("click", startStopwatch);
         resetButton.addEventListener("click", resetStopwatch);
 
-    function saveHTML() {
-      const contentElement = document.getElementById('history-list');
-      const contentText = contentElement.innerText; // コンテンツのテキストを取得
-      const blob = new Blob([contentText], { type: 'text/plain' });
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = 'content.txt';
-      link.click();
-      URL.revokeObjectURL(link.href);
-    }
+        function saveHTML() {
+          const contentElement = document.getElementById('history-list');
+          let contentText = contentElement.innerText; // コンテンツのテキストを取得
+        
+          // "追記" を含む行を削除
+          contentText = contentText.split('\n').filter(line => !line.includes('追記')).join('\n');
+          
+          const blob = new Blob([contentText], { type: 'text/plain' });
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(blob);
+          link.download = 'content.txt';
+          link.click();
+          URL.revokeObjectURL(link.href);
+        }
 
     // リセットボタンの処理
     function resetContent() {
