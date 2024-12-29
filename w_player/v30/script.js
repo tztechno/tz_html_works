529772d7927d19a3f37dc8982d37064fd48b2807
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-12-29 22:29:03 +0900

    Add files via upload

--- w_player/v30/script.js
+++ w_player/v30/script.js
@@ -265,14 +265,15 @@ function saveHTML() {
     const formattedDate = now.toLocaleString(); // ローカル日時形式
 
     // 保存するテキストを作成
-    const finalText = `タイム:\n${formattedDate}\n\n` +
+    const finalText = `日時:\n${formattedDate}\n\n` +
+        `タイム:\n${stopWatchTime}\n\n` +
         `履歴:\n${historyLines.filter(line => line.trim() !== '追記').join('\n')}\n\n` +
         `ベンチ:\n${benchText.split('\n').filter(line => line.trim() !== 'ベンチ').join('\n')}\n\n` +
         `ピッチ:\n${fieldText.split('\n').filter(line => line.trim() !== 'ピッチ').join('\n')}\n\n` +
         `ピッチ外:\n${tempOutText.split('\n').filter(line => line.trim() !== 'ピッチ外').join('\n')}\n\n` +
         `禁止プレイヤー:\n${forbiddenPlayer.join(', ')}\n\n` +
-        `保留中の選手交代:\n${pendingSubstitutionsText || 'なし'}\n\n` +
-        `保留中の選手移動:\n${pendingMovesText || 'なし'}\n\n`;
+        `保留中の選手交代:\n${pendingSubstitutionsText}\n\n` +
+        `保留中の選手移動:\n${pendingMovesText}\n\n`;
 
     // テキストを保存
     const blob = new Blob([finalText], { type: 'text/plain' });
@@ -1023,5 +1024,13 @@ function executePendingSub(index) {
 
 
 ////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+
 
 
